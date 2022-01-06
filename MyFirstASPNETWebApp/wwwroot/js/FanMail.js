330788c7e68aae4909ae00c278cb14c4007a5d0c
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-05 19:54:57 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,6 @@
+﻿$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+    $("#picGanderson").fadeToggle();
+})
